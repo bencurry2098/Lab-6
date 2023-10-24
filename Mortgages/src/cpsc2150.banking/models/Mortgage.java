6e1f5a9b19682f0b73d48e62dde50afc0456efa9
Cameron Scott <casctt@clemson.edu>
2023-10-24 10:25:53 -0400

    added contract setup to mortgage.java

--- Mortgages/src/cpsc2150.banking/models/Mortgage.java
+++ Mortgages/src/cpsc2150.banking/models/Mortgage.java
@@ -1,4 +1,16 @@
+/**
+ * Cayden Akers
+ * Ben Curry
+ * Cameron Scott
+ */
 package cpsc2150.banking.models;
+
+/** MortgageClassContract
+ *
+ * @invariant:
+ *
+ * @corresponds:
+ */
 public class Mortgage extends AbsMortgage implements IMortgage{
 
     
